61325e7ea3f6cc7b71799e6fa22007e2e982ca0c
andyh0913 <andyh0913@gmail.com>
2021-06-25 09:03:12 +0800

    bind to 0.0.0.0

--- server.py
+++ server.py
@@ -106,7 +106,7 @@ def query_cb(sid, query):
 # start server
 
 log_file_name = 'server_log.txt'
-address = '127.0.0.1'
+address = '0.0.0.0'
 port = 8080
 green_socket = eventlet.listen((address, port))
 print(f'Server is online at {address}:{port}')
